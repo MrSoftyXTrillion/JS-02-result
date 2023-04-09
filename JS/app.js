e4e45e45a62941770d5aff92c726bcc8094ed7d9
Mr.SoftyXTrillion <123297701+MrSoftyXTrillion@users.noreply.github.com>
2023-04-10 02:21:04 +0600

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,23 @@
+function result (physicsMark , chemistryMark , highermathMark , biologyMark , userName) {
+    let physics = physicsMark
+    let chemistry = chemistryMark
+    let highermath = highermathMark
+    let biology = biologyMark
+    let user = userName
+    let result = physics + chemistry + highermath + biology 
+console.log (`Hello ${user} , Your Result Has been Published. Here is your total mark ==> ${result}.`)
+}
+result(190 , 192 , 155 , 196 , `Motaher Vai`)
+result(189 , 132 , 195 , 196 , `Rowfun Vai`)
+result(188 , 132 , 195 , 196 , `Ratul Vai`)
+result(187 , 132 , 195 , 196 , `Ridwan Vai`)
+result(186 , 132 , 195 , 196 , `Ibrahim Vai`)
+result(185 , 132 , 195 , 196 , `Sakib Vai`)
+result(184 , 132 , 195 , 196 , `Amir Vai`)
+result(183 , 132 , 195 , 196 , `Juboraaj Vai`)
+result(181 , 132 , 195 , 196 , `Safa Apu`)
+result(180 , 132 , 195 , 196 , `Sayema Apu`)
+result(179 , 132 , 195 , 196 , `Abida Apu`)
+result(178 , 132 , 195 , 196 , `Abida Apu 2`)
+result(182 , 132 , 195 , 196 , `Khaled`)
+result(182 , 132 , 195 , 196 , `Jowel Vai`)
